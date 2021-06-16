cdc238bccfb8753a733e8e00594761e63943d7c6
David Hazy <support@auth.gg>
2021-06-15 22:43:29 -0700

    Annual SSL Update

--- api/authgg.php
+++ api/authgg.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION))
 class AuthGG
 {
 
-    private static $certkey = "sha256//2hVOtsSzxX/xZP9CI17EN/XTiP3PbZwCPAXNuXhHJIA=";
+    private static $certkey = "sha256//kM1Uss9aoNHOZ2i4t8h18vi+TgoDX+fu/1yRU7yLNYE=";
     private static $api = "https://api.auth.gg/php/";
 
     public static function error($message)
